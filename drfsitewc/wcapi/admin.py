886ce318309caf634946e95739a7d390b2137870
psiap <TVpsiap@mail.ru>
2023-02-27 13:27:01 +0300

    * AddLoginsAuth

--- drfsitewc/wcapi/admin.py
+++ drfsitewc/wcapi/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Statistics, Settings, History
+from .models import Statistics, Settings, History, Logins
+
 # Register your models here.
 admin.site.register(Statistics)
 admin.site.register(Settings)
 admin.site.register(History)
+admin.site.register(Logins)
